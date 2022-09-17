6607a818e7680e015dcd1b3f1e7aa0a5c4b18adb
Viktor Timofeev <uskusstbo2@yandex.ru>
2022-09-18 00:21:27 +0300

    4

--- src/components/Card.js
+++ src/components/Card.js
@@ -21,6 +21,7 @@ export class Card {
     this._image.src = this._link;
     this._image.alt = this._text;
     this._title.textContent = this._text;
+    console.log(this._title.textContent);
     this._updateLikesView;
     this._setEventListeners();
  //   this._trash.classList.add(this._userId === this._ownerId ? 'card__delete-button_visible': 'card__delete-button_hidden');
