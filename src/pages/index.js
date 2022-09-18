9cfda9ef92113d76a875b84c846a592362cb23fa
Viktor Timofeev <uskusstbo2@yandex.ru>
2022-09-19 00:46:46 +0300

    4

--- src/pages/index.js
+++ src/pages/index.js
@@ -141,14 +141,14 @@ const createCard = (cardData) => {
         },
         handleLikeCard: (card) => {
         //  this._like.classList.toggle("elements__like_state_active");
-          if (card.isLiked) {
-            api.likeCard(card.id())
+          if (card.isLiked()) {
+            api.deleteLike(card.id())
             .then(data => {
               card.setLikesInfo({...data});
             })
             .catch(error => console.log(`Ошибка изменения статуса лайка: ${error}`))
           } else {
-            api.deleteLike(card.id())
+            api.likeCard(card.id())
             .then(data => {
               card.setLikesInfo({...data});
             })
