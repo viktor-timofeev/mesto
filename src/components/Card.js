ac843fcdbe8d13aa5f6af517d6627aff2b34bd68
Viktor Timofeev <uskusstbo2@yandex.ru>
2022-09-20 19:55:59 +0300

    4

--- src/components/Card.js
+++ src/components/Card.js
@@ -36,9 +36,9 @@ export class Card {
   _updateLikesView() {
     if (!this.isLiked) {
       this._like.classList.remove("elements__like_state_active");    
-    }/* else {
+    } else {
       this._like.classList.add("elements__like_state_active"); 
-    }*/
+    }
   }
 
   setLikesInfo(newLikes) {
