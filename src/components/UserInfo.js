209edd5994adc0c2029d798eb7b32f3ae110542e
Viktor Timofeev <uskusstbo2@yandex.ru>
2022-09-26 02:27:19 +0300

    4

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -16,6 +16,7 @@ export class UserInfo {
   setUserInfo(data) {
     this._profileName.textContent = data.userName;
     this._profileInfo.textContent = data.userDescription;
+
   }
 
   setUserPic(data) {
